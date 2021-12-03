4bb4bf1d0c5b3b56644f229b7cf61b95ac9ec60c
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-12-03 23:53:35 +0000

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let lines: Vec<_> = input.lines().collect();
+    let mut prev = None;
+    let mut more = 0;
+    for line in lines {
+        let depth = line.parse::<u32>().unwrap();
+        if let Some(prev) = prev {
+            if depth > prev {
+                more += 1;
+            }
+        }
+        prev = Some(depth);
+    }
+    println!("{}", more);
+}
