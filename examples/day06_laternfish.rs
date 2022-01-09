bc9502da9a1937320a7a1a1d2c103afe6db5b22d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-01-09 02:01:10 +0000

    Day 6

--- examples/day06_laternfish.rs
+++ examples/day06_laternfish.rs
@@ -1,28 +1,48 @@
 const TEST_INPUT: &str = "3,4,3,1,2";
 const INPUT: &str = "3,5,4,1,2,1,5,5,1,1,1,1,4,1,4,5,4,5,1,3,1,1,1,4,1,1,3,1,1,5,3,1,1,3,1,3,1,1,1,4,1,2,5,3,1,4,2,3,1,1,2,1,1,1,4,1,1,1,1,2,1,1,1,3,1,1,4,1,4,1,5,1,4,2,1,1,5,4,4,4,1,4,1,1,1,1,3,1,5,1,4,5,3,1,4,1,5,2,2,5,1,3,2,2,5,4,2,3,4,1,2,1,1,2,1,1,5,4,1,1,1,1,3,1,5,4,1,5,1,1,4,3,4,3,1,5,1,1,2,1,1,5,3,1,1,1,1,1,5,1,1,1,1,1,1,1,2,2,5,5,1,2,1,2,1,1,5,1,3,1,5,2,1,4,1,5,3,1,1,1,2,1,3,1,4,4,1,1,5,1,1,4,1,4,2,3,5,2,5,1,3,1,2,1,4,1,1,1,1,2,1,4,1,3,4,1,1,1,1,1,1,1,2,1,5,1,1,1,1,2,3,1,1,2,3,1,1,3,1,1,3,1,3,1,3,3,1,1,2,1,3,2,3,1,1,3,5,1,1,5,5,1,2,1,2,2,1,1,1,5,3,1,1,3,5,1,3,1,5,3,4,2,3,2,1,3,1,1,3,4,2,1,1,3,1,1,1,1,1,1";
 
-fn update_state(state: &[u32]) -> Vec<u32> {
-    let mut r = Vec::new();
-    for &x in state {
+#[derive(Default)]
+struct State {
+    count_by_day: [u64; 9],
+}
+
+impl State {
+    fn count(&self) -> u64 {
+        self.count_by_day.iter().map(|x| *x as u64).sum()
+    }
+}
+
+fn update_state(state: &State) -> State {
+    let mut r = State::default();
+    for (x, &count) in state.count_by_day.iter().enumerate() {
         if x != 0 {
-            r.push(x - 1);
+            r.count_by_day[x - 1] += count;
         } else {
-            r.push(6);
-            r.push(8);
+            r.count_by_day[6] += count;
+            r.count_by_day[8] += count;
         }
     }
     r
 }
 
 fn run(input: &str) {
-    let mut state = input
+    let state_raw = input
         .split(",")
         .map(|x| x.parse::<u32>().unwrap())
         .collect::<Vec<u32>>();
+    let mut state = State::default();
+    for &x in &state_raw {
+        state.count_by_day[x as usize] += 1;
+    }
+
     for _ in 0..80 {
         state = update_state(&state);
     }
-    println!("{}", state.len());
+    println!("day 80: {}", state.count());
+    for _ in 80..256 {
+        state = update_state(&state);
+    }
+    println!("day 256: {}", state.count());
 }
 
 fn main() {
